2ce2b579b16070ea18e0390ee17e4e1e74ba70eb
Higor <higorvitorpadilha@gmail.com>
2024-09-19 15:39:03 -0300

    main

---
+++ .venv/Tipovariavel.py
@@ -0,0 +1,8 @@
+codigo=10
+salario=1500.00
+nome='jose'
+situacao= True
+
+tipo = type(salario)
+
+print("codigo:", codigo, "nome:", nome, 'o salario atual é :',salario)
---
+++ .venv/exibirvariavel.py
@@ -0,0 +1,6 @@
+codigo=105
+nome= 'jose santana'
+salario= 1650.00
+ativo= True
+
+print('codigo: %d'% codigo)
