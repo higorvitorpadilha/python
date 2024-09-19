2a980b6522be667db8a650f0d1b49f95d046ffb1
Higor <higorvitorpadilha@gmail.com>
2024-09-19 15:54:00 -0300

    main

---
+++ .venv/entrada de dados.py
@@ -0,0 +1,13 @@
+fruta= input("digite o nome de uma  fruta: ")
+print(fruta)
+
+
+codigo = int (input ("Digite o código do funcionario: "))
+nome= input("Qual o nome do funcionario? ")
+salario=float (input("informe o salario "))
+ativo= True
+
+print("Código: ", codigo)
+print("nome: ", nome)
+print("salario: ", salario)
+print("ativo: ", ativo)
